6a9f320866cb268ac20e68f8794d73c9b70a6a28
Yohan <66806606+siixty@users.noreply.github.com>
2022-01-27 21:16:12 +0100

    +

--- main.js
+++ main.js
@@ -1,37 +1,72 @@
-import * as THREE from 'https://threejs.org/build/three.module.js';
+import * as THREE from 'three';
 
 import {
   OrbitControls
-} from 'https://threejs.org/examples/jsm/controls/OrbitControls.js';
+} from 'three/examples/jsm/controls/OrbitControls.js';
+
+import {
+  RGBELoader
+} from 'three/examples/jsm/loaders/RGBELoader.js'
+
+import {
+  sRGBEncoding
+} from 'three';
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 2500);
 
 const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector("#bg"),
   button: document.getElementById("btn2"),
-  antialias: true,
+  powerPreference: "high-performance",
+  antialias: true
 })
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
-camera.position.setZ(200);
+camera.position.set(0, 35, 70);
+
+
 
 renderer.render(scene, camera);
 
+window.addEventListener("resize", () => {
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  camera.aspect = window.innerWidth / window.innerHeight;
+
+  camera.updateProjectionMatrix();
+})
+
 // Geometry
 
-let diffMap = new THREE.TextureLoader().load("diffMap.png");
-let normalMap = new THREE.TextureLoader().load("normalDX.png");
-normalMap.wrapS = THREE.RepeatWrapping;
-normalMap.wrapT = THREE.RepeatWrapping;
-let roughMap = new THREE.TextureLoader().load("roughMap.png");
-let dispMap = new THREE.TextureLoader().load("dispMap.png");
-let aoMap = new THREE.TextureLoader().load("aoMap.png");
+let diffMap = new THREE.TextureLoader().load("./graphics/planet_textureAzure.jpg");
+diffMap.encoding = sRGBEncoding;
+
+
+let roughMap = new THREE.TextureLoader().load("./graphics/planet_textureAuric.jpg");
+roughMap.encoding = sRGBEncoding;
+let dispMap = new THREE.TextureLoader().load("./graphics/planet_textureChondrite.jpg");
+dispMap.encoding = sRGBEncoding;
+let aoMap2 = new THREE.TextureLoader().load("./graphics/planet_textureBurnt.jpg");
+aoMap2.encoding = sRGBEncoding;
+
+let map5 = new THREE.TextureLoader().load("./graphics/planet_textureBlueGiant.jpg");
+map5.encoding = sRGBEncoding;
+let map6 = new THREE.TextureLoader().load("./graphics/planet_textureCyanic.jpg");
+map6.encoding = sRGBEncoding;
+let map7 = new THREE.TextureLoader().load("./graphics/planet_textureChlorine.jpg");
+map7.encoding = sRGBEncoding;
+
+let map8 = new THREE.TextureLoader().load("./graphics/planet_textureDust.jpg");
+map8.encoding = sRGBEncoding;
+let map9 = new THREE.TextureLoader().load("./graphics/planet_textureDesertic.jpg");
+map9.encoding = sRGBEncoding;
+let map10 = new THREE.TextureLoader().load("./graphics/planet_textureFluorescent.jpg");
+map10.encoding = sRGBEncoding;
 
 
 let matArray = [
-  diffMap, normalMap, roughMap, dispMap, aoMap
+  diffMap, roughMap, dispMap, aoMap2, map5, map6, map7, map8, map9, map10
 ]
 
 
@@ -42,36 +77,58 @@ let matArray = [
 // roughnessMap: roughMap,
 // displacementMap: dispMap,
 // aoMap: aoMap
+
+
+const hdrEquirect = new RGBELoader().load(
+  "satara_night_2k.hdr",
+  () =>
+  hdrEquirect.mapping = THREE.EquirectangularReflectionMapping
+
+)
+
 let dodeMat = new THREE.MeshPhysicalMaterial({
-  map: dispMap,
-  bumpMap: dispMap,
-  displacementMap: dispMap,
-  aoMap: dispMap
+  map: diffMap,
+  displacementMap: diffMap,
+  aoMap: diffMap,
+  bumpMap: diffMap,
+  bumpScale: .5,
+  // normalMap: diffMap,
+  // envMap: hdrEquirect,
 })
 let dode = new THREE.Mesh(
-  new THREE.DodecahedronGeometry(50, 20),
+  new THREE.SphereBufferGeometry(10, 50, 50),
   dodeMat
 )
 btn2.addEventListener("click", function () {
   var rgb = [Math.floor(Math.random() * 256), Math.floor(Math.random() * 256), Math.floor(Math.random() * 256)];
 
+
+  // DODEMAT
   dodeMat.displacementMap = matArray[Math.floor(Math.random() * matArray.length)];
   dodeMat.aoMap = matArray[Math.floor(Math.random() * matArray.length)];
-  dodeMat.aoMapIntensity = Math.floor((Math.random() * 10) + 1);
   dodeMat.displacementScale = Math.floor((Math.random() * 10) + 1);
-  dodeMat.bumpScale = Math.floor((Math.random() * 3) + 0);
   dodeMat.map = matArray[Math.floor(Math.random() * matArray.length)];
   dodeMat.bumpMap = matArray[Math.floor(Math.random() * matArray.length)];
   dodeMat.color.set('rgb(' + rgb.join(', ') + ')')
 });
 
-scene.add(dode);
 
-const pointLight = new THREE.PointLight(0xffffff, 1.5);
-pointLight.position.set(500, 500, 500);
+// SATELLITE
+const moonMat = new THREE.TextureLoader().load("/graphics/3Uq2YE8l.jpg")
+const normalMoonMat = new THREE.TextureLoader().load("./normalDX.png")
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.1);
-scene.add(pointLight, ambientLight);
+var moonGeometry = new THREE.SphereBufferGeometry(3.5, 50, 50);
+var moonMaterial = new THREE.MeshPhongMaterial({
+  map: moonMat,
+  normalMap: normalMoonMat
+});
+var moon = new THREE.Mesh(moonGeometry, moonMaterial);
+moon.position.set(50, 0, 0);
+
+var ambientLight = new THREE.AmbientLight(0xf1f1f1);
+
+var spotLight = new THREE.DirectionalLight(0xffffff);
+spotLight.position.set(50, 50, 50);
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enablePan = false;
@@ -86,10 +143,13 @@ controls.rotateSpeed = 0.07;
 
 function addStar() {
 
-  const geometry = new THREE.SphereGeometry(1, 24, 24);
-  const material = new THREE.MeshPhysicalMaterial({
-    color: 0xffd27d,
-    clearcoat: 1,
+  let starMatNR = new THREE.TextureLoader().load("./graphics/star_textureCustomBlue.jpg");
+  starMatNR.wrapS = THREE.RepeatWrapping;
+  starMatNR.wrapT = THREE.RepeatWrapping;
+
+  const geometry = new THREE.SphereBufferGeometry(0.5, 24, 24);
+  const material = new THREE.MeshStandardMaterial({
+    normalMap: starMatNR,
   })
 
   const star = new THREE.Mesh(geometry, material);
@@ -97,10 +157,42 @@ function addStar() {
   const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(1000));
 
   star.position.set(x, y, z);
-  scene.add(star, pointLight);
-}
-Array(600).fill().forEach(addStar);
+  scene.add(star, spotLight);
 
+}
+Array(500).fill().forEach(addStar);
+
+
+// // function addPlanet() {
+
+// //   const geometry = new THREE.DodecahedronGeometry(25, 6);
+// //   const material = new THREE.MeshPhysicalMaterial({
+// //     map: diffMap,
+// //     // bumpMap: dispMap,
+// //     // displacementMap: dispMap,
+// //     // aoMap: dispMap,
+// //     envMap: hdrEquirect,
+// //   })
+
+// //   const planet = new THREE.Mesh(geometry, material)
+
+// //   const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(2500));
+
+// //   planet.position.set(x, y, z);
+// //   scene.add(planet, pointLight);
+
+// btn2.addEventListener("click", function () {
+//   material.displacementMap = matArray[Math.floor(Math.random() * matArray.length)];
+//   material.aoMap = matArray[Math.floor(Math.random() * matArray.length)];
+//   material.aoMapIntensity = Math.floor((Math.random() * 10) + 1);
+//   material.displacementScale = Math.floor((Math.random() * 10) + 1);
+//   material.bumpScale = Math.floor((Math.random() * 3) + 0);
+//   material.map = matArray[Math.floor(Math.random() * matArray.length)];
+//   material.bumpMap = matArray[Math.floor(Math.random() * matArray.length)];
+//   material.color.set('rgb(' + rgb.join(', ') + ')')
+// })
+// }
+// Array(25).fill().forEach(addPlanet);
 
 // const spaceTexture = new THREE.TextureLoader().load('space.jpg');
 // scene.background = spaceTexture;
@@ -108,13 +200,33 @@ Array(600).fill().forEach(addStar);
 
 // ANIMATION
 
+//Set the moon's orbital radius, start angle, and angle increment value
+var r = 35;
+var theta = 0;
+var dTheta = 2 * Math.PI / 1000;
+
 function animate() {
   requestAnimationFrame(animate);
 
-  dode.rotation.z -= 0.004;
+  dode.rotation.y += .0005;
+
+  theta += dTheta;
+  moon.position.x = r * Math.cos(theta);
+  moon.position.z = r * Math.sin(theta);
 
   controls.update();
 
   renderer.render(scene, camera);
 }
 animate();
+
+
+scene.add(spotLight);
+scene.add(ambientLight);
+scene.add(moon);
+scene.add(dode);
+
+console.log("Scene polycount:", renderer.info.render.triangles)
+console.log("Active Drawcalls:", renderer.info.render.calls)
+console.log("Textures in Memory", renderer.info.memory.textures)
+console.log("Geometries in Memory", renderer.info.memory.geometries)
